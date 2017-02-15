d00d489765cb882f686a4e4d28d0e371e4299cd8
bobozhang <emilyzhangbo@163.com>
2017-02-15 13:00:36 +0800

    提交信息

---
+++ src/com/javaIO/test/TestMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.javaIO.test;
+
+/**
+ * @author zhangbo
+ * 2017年2月15日  下午12:57:26
+ * 
+ */
+public class TestMain {
+
+	/**
+	 * @author  zhangbo
+	 * 2017年2月15日  下午12:57:28
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !!!");
+	}
+
+}
